80691ece8c0fdf7594894e53c7ba695f9a9aa203
Kevien <307572384@qq.com>
2019-01-26 19:07:41 +0800

    删除无用的fragment关于单对单传值

--- app/src/main/java/com/beta/fragment/MainActivity.java
+++ app/src/main/java/com/beta/fragment/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 
 import com.beta.fragment.Activity.BEActivity;
-import com.beta.fragment.Activity.CZActivity;
 import com.beta.fragment.Activity.FWXActivity;
 import com.beta.fragment.Activity.TActivity;
 import com.beta.fragment.Activity.TDActivity;
@@ -16,7 +15,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 	private Button tf_1_low1;
 	private Button tf_1_low2;
-	private Button tf_low3;
 	private Button tf_low4;
 	private Button tf_low5;
 
@@ -33,8 +31,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		tf_1_low1.setOnClickListener(this);
 		tf_1_low2 = (Button) findViewById(R.id.tf_1_low2);
 		tf_1_low2.setOnClickListener(this);
-		tf_low3 = (Button) findViewById(R.id.tf_low3);
-		tf_low3.setOnClickListener(this);
 		tf_low4 = (Button) findViewById(R.id.tf_low4);
 		tf_low4.setOnClickListener(this);
 		tf_low5 = (Button) findViewById(R.id.tf_low5);
@@ -51,11 +47,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 			case R.id.tf_1_low2://静态Fragment
 				Intent low2_intent = new Intent(MainActivity.this, TDActivity.class);
 				startActivity(low2_intent);
-				break;
-
-			case R.id.tf_low3:
-				Intent low3_intent = new Intent(MainActivity.this, CZActivity.class);
-				startActivity(low3_intent);
 				break;
 			case R.id.tf_low4://EventBus实现fragment之间数据通讯
 				Intent low4_intent = new Intent(MainActivity.this, BEActivity.class);
